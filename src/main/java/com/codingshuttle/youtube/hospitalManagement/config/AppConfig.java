c905c0a6abc7383a14b3012ba142b2ef58138b4a
Tushar Jamadar <tusharjamadar2001@gmail.com>
2025-09-24 00:00:50 +0530

    spring security lecture 1

--- src/main/java/com/codingshuttle/youtube/hospitalManagement/config/AppConfig.java
+++ src/main/java/com/codingshuttle/youtube/hospitalManagement/config/AppConfig.java
@@ -3,6 +3,10 @@ package com.codingshuttle.youtube.hospitalManagement.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.net.PasswordAuthentication;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +14,10 @@ public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
